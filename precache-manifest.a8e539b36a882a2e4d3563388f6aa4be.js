49bfe0071d2b033d2806bdfe47bae41b80791733
beresnewa <ekaretinaberesneva5@gmail.com>
2020-06-18 18:17:56 +0300

    Updates

---
+++ precache-manifest.a8e539b36a882a2e4d3563388f6aa4be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b33fc24ab333bd19fe1ad0416737e5e",
+    "url": "/NASA_APOD/index.html"
+  },
+  {
+    "revision": "b46c0cfe8b71a2c23fb6",
+    "url": "/NASA_APOD/static/css/main.87c0379c.chunk.css"
+  },
+  {
+    "revision": "63fd9e2650f582292820",
+    "url": "/NASA_APOD/static/js/2.550d1153.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NASA_APOD/static/js/2.550d1153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46c0cfe8b71a2c23fb6",
+    "url": "/NASA_APOD/static/js/main.405b44f6.chunk.js"
+  },
+  {
+    "revision": "2eb5485389bc1c0fc844",
+    "url": "/NASA_APOD/static/js/runtime-main.88421bcb.js"
+  },
+  {
+    "revision": "e648944d5d2026cae8c23c93ce978efa",
+    "url": "/NASA_APOD/static/media/background-fon.e648944d.jpg"
+  }
+]);
