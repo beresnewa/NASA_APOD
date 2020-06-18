5004ce2cc9d5ed9a79ca273fa979e1fac9a8e3cd
beresnewa <ekaretinaberesneva5@gmail.com>
2020-06-18 18:18:48 +0300

    addProject3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Apod from './src/components/Apod/index';
+import Apod from './components/Apod/index';
 import './index.css';
 
 ReactDOM.render(
